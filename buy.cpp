e97ecc5029fa5e7fdd648bead33d3e48ea46ec8f
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-12 23:35:39 +0530

    Add files via upload

---
+++ buy.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+ int main(){
+    int total,bit,lite,eth,;
+cout<< "enter the total money";
+cin >>total;
+cout<< "enter price of one bitcoin";
+cin>>bit;
+cout<< "enter price of one etherium ";
+cin>>eth;
+cout<< "enter price of on litecoin";
+cin>>lite;
+ 
+int bits=total/bit;
+cout<<"number of btcoin you can buy = "<< bits << ends;
+
+int  eths=total/eth;
+cout<<"number of etherium you can buy = "<< eths << ends;
+
+int  lites=total/lite;
+cout<<"number of litecoin you can buy = "<< lites << ends;
+
+
+
+
+return 0;
+
+
+
+ }

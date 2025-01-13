6b877331f3036dc52fe684bafd1a3259b06d5033
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-13 19:49:01 +0530

    Update annualwage.cpp

--- annualwage.cpp
+++ annualwage.cpp
@@ -1,3 +1,14 @@
+The exercise is almost identical to a previous exercise with a minor change. In a company the monthly salary of an employee is calculated by minimum wage 400$
+per month, plus 20$ multiply by the employment years, plus 30$ for each employee kid, plus 100$ if the employee didn't miss 1 day of work.
+
+Create a program that:
+
+Reads the employment years
+Reads the number of each employee kids
+Prints the total amount the employee must take
+Output: "The total amount is 660$, 400$ minimum wage + 100$ for 5 years experience + 60$ for 2 kids + 100$ for not missing a day at work"
+
+
 #include <iostream>
 using namespace std;
 

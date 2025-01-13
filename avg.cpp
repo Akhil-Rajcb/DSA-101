62a76d49fc6e2b1a5816169d149e2fc0237a3692
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-13 19:51:36 +0530

    Update avg.cpp

--- avg.cpp
+++ avg.cpp
@@ -1,3 +1,14 @@
+It's the end of the semester and you got your grades from three classes: Geometry, Algebra, and Physics.
+
+Create a program that:
+
+Reads the grades of these 3 classes (Grades range from 0 - 10)
+Calculate the average of your grades
+Example: Geometry = 6, Algebra = 7, Physics = 8
+Output: average_score = 7
+Warning! Do not use the programming language MAGIC. After you complete the exercise feel free to do so.
+
+
 #include <iostream>
 int main(){
 int a,b,c,avg,sum;

b6eaec8ef7bd7cb4a367ec2f9f218acd7e9c667e
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-13 19:47:21 +0530

    Add files via upload

---
+++ secondlastnumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int number;
+cout<<"enter the number = ";
+cin>>number;
+int lastNumber=number/10;
+int scNumber=lastNumber%10;
+
+cout<<"second last number = "<<scNumber<< ends;
+return 0;
+}

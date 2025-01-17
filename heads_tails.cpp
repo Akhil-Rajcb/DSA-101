0d296f80293e06acacccfad3866c50c0b5cf932c
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-17 21:13:55 +0530

    Update heads_tails.cpp

--- heads_tails.cpp
+++ heads_tails.cpp
@@ -1,3 +1,18 @@
+You start flipping a coin, count and print how many times the result was head or tails until you enter the word "stop".
+Then find and print the percentage of how many head or tails was the result.
+
+Create a program that:
+
+Reads if the flipped coin was head or tails
+If the value is "stop", print proper message and quit program
+While value not "stop", count the result
+Print the proper message
+Calculates the percentage of head and tails
+Prints the proper message
+Input: "head", "tails", "tails", "tails", "head", "head", "tails", "tails", "tails", "head"
+Ouput: "Head won 4 times and tails won 6 times"
+Output: "40% Head, 60% Tails"
+
 #include<iostream>
 using namespace std;
 int main(){

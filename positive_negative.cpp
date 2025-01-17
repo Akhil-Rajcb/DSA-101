dfc6ccdbc1b5598c3e95e1930cb7b347c081c977
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-17 21:12:39 +0530

    Update positive_negative.cpp

--- positive_negative.cpp
+++ positive_negative.cpp
@@ -1,3 +1,12 @@
+Create a program that reads 5 numbers and prints if the number is negative or positive
+
+Create a program that:
+
+Reads the 5 numbers you want
+Print if a number is negative or positive
+Input example: 4, 6, -11, -4, 9
+Ouput: "Positive", "Positive", "Negative", "Negative", "Positive",
+
 #include<iostream>
 using namespace std;
 int main(){

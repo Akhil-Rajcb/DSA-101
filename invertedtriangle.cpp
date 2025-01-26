8f79e663bee4a01c57650f4a9d35931d8cf29ba8
Akhil-Rajcb <149132367+Akhil-Rajcb@users.noreply.github.com>
2025-01-26 13:44:04 +0530

    Add files via upload

---
+++ invertedtriangle.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int i,j,n;
+    
+
+    cout<<"enter the no of lines = ";
+    cin>>n;
+
+    for(i=0;i<n;i++) {
+        if(i%2==0) {
+            int noOfStars = n-i; 
+            int noOfSpace = n-noOfStars;
+            int noOfHalfSpace = noOfSpace/2;
+
+            for(j=0;j<noOfHalfSpace+1;j++) {
+                cout<<"\t";
+            }
+            for(j=0;j<noOfStars;j++) {
+                cout<<"\t *";
+            }
+            for(j=0;j<noOfHalfSpace+1;j++) {
+                cout<<"\t";
+            }
+            
+            
+        }else{
+            for(j=0;j<n;j++){
+                cout<<"\t";
+            }
+        }
+        cout<<"\n";
+
+    }
+    
+    return 0;
+}
